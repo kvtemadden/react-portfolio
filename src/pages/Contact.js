16988535378b6d2295fd4daab39f92faec828d4b
kate <katy.madden99@gmail.com>
2021-05-15 14:29:03 +0100

    updated class to className

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,25 +6,25 @@ export function Contact() {
   return (
     <>
     <Header page="Contact"/>
-<section class="mx-auto mt-5 container">
-<div class="row bg">
-  <h2 class="col-12">Get in touch!</h2>
-    <form action="form-submit.html" method="POST" class="col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
-      <div class="form-group">
+<section className="mx-auto mt-5 container">
+<div className="row bg">
+  <h2 className="col-12">Get in touch!</h2>
+    <form action="form-submit.html" method="POST" className="col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl-8">
+      <div className="form-group">
         <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" placeholder="Your name"/>
+        <input type="text" className="form-control" id="name" placeholder="Your name"/>
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="emailAddress">Email address</label>
-        <input type="email" class="form-control" id="emailAddress" placeholder="name@example.com"/>
+        <input type="email" className="form-control" id="emailAddress" placeholder="name@example.com"/>
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="message">Your message</label>
-        <textarea class="form-control" id="message" rows="3"></textarea>
+        <textarea className="form-control" id="message" rows="3"></textarea>
       </div>
-      <button type="submit" class="btn btn-pink">Send Message</button>
+      <button type="submit" className="btn btn-pink">Send Message</button>
     </form>
-    <p class="col-12 mt-5">07123456789  |  <a href="mailto:kate@email.com">kate@email.com</a></p>
+    <p className="col-12 mt-5">07123456789  |  <a href="mailto:kate@email.com">kate@email.com</a></p>
   </div>
 </section>
 </>
